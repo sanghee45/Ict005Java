11aad9f01f9039f119e7f226e04ee1140fb8ebfa
sanghee45 <101611814+sanghee45@users.noreply.github.com>
2022-09-24 15:23:52 +0900

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,17 @@
+package sanghee;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(10+20);
+		System.out.println(20-10);
+		System.out.println(10*20);
+		System.out.println(20/10);	
+		
+		
+		
+
+	}
+
+}
